de199aed4d30f46b467d7b9d151687122b07727a
DanyloMatskiv <matskiv0601@gmail.com>
2024-12-01 23:20:15 +0200

    Add files via upload

---
+++ # from ipaddress import collapse_address.py
@@ -0,0 +1,152 @@
+# from ipaddress import collapse_addresses
+
+
+# print('Hello World')
+
+# # Тип зміної float нецілі числа
+# Price = 9.99
+
+# # Тип зміної int цілі числа
+# number = 10
+
+# # Тип зміної String рядок або текст
+# name = 'Danylo'
+
+# # Тип зміної bool (boolsed) 
+# light =True
+# # 
+
+
+
+
+# fanta = 1.25
+# cola = 0.5
+
+# year = 13
+# old = 3
+
+
+# surname = 'I'
+# name = 'Malpha'
+
+
+# fan = False
+# night =True
+
+
+
+# print(fanta)
+# print(cola)
+# print(year)
+# print(name)
+# print(old)
+# print(surname)
+# print(fan)
+# print(night)
+
+
+
+
+# print(type(fanta))
+# print(type(cola))
+# print(type(year))
+# print(type(old))
+# print(type(surname))
+# print(type(name))
+# print(type(fan))
+# print(type(night))
+
+
+
+
+# cola =1.50
+# fanta = 0.750
+
+# if cola > 1:
+#     print("You won't get druk")
+# elif fanta > 1:
+#     print("You won't get druk")
+# else:
+#      print("you got drunk")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range (10):
+#     print('Hello World')
+
+
+# a =5
+
+
+# while a < 10:
+#     a += 1
+#     print(a)
+
+
+
+
+
+
+
+# for i in range (12):
+#     print('Hi')
+
+
+
+
+# apple = 4
+# banana = 3
+
+# if apple > 3:
+  
+#  while apple < 2:
+#      apple += 1
+#      print("You lose")
+
+# for i  in range (12):
+#      print ("You lose") 
+
+
+# elif banana > 3:
+
+#  while banana < 2:
+#      banana += 1
+#      print("You lose")
+
+# for i  in range (12):
+#      print ("You lose") 
+
+# else:
+#      print("you win")
+
+
+
+
+
+
+
+
+sprite = 1.25
+cola = 0.5
+
+year = 13
+old = 3
+
+
+surname = 'Matskiv'
+name = 'Malpha'
+
+
+fan = False
+night =True
